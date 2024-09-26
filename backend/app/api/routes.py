99ffeb8c5581618e3bc6fabae4d4116dc3906b00
maxitech <derstadlmax@gmail.com>
2024-09-26 11:35:35 +0200

    Added functions to get_all_notes, get_note_by_id, create_note and _save_notes. Createt necessary api endpoints for the functions.

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import RedirectResponse
 
+from db import NoteRepository
+from schemas import NoteSchema
+
+
+notes_repo = NoteRepository()
+
+
 router = APIRouter()
+
 
 @router.get('/')
 def read_root():
@@ -10,4 +18,20 @@ def read_root():
 
 @router.get('/notes')
 def get_notes():
-    return {'note': 'Hi, I am a note'}
+    notes = notes_repo.get_all_notes()
+    if notes: 
+        return notes
+    raise HTTPException(status_code=404, detail='No notes yet')
+
+
+@router.get('/notes/{note_id}')
+def get_note(note_id: str):
+    note = notes_repo.get_note_by_id(note_id=note_id)
+    if note: 
+        return note
+    raise HTTPException(status_code=404, detail='Note not found')
+
+
+@router.post('/notes')
+def create_note(note: NoteSchema):
+    return notes_repo.create_note(note=note)
--- backend/app/db/notes_repository.py
+++ backend/app/db/notes_repository.py
@@ -1,18 +1,42 @@
 import json 
+import os
+from uuid import uuid4
+
 from schemas import NoteSchema
 
 class NoteRepository: 
     def __init__(self, filename='notes.json') -> None:
-        pass
+        self.filename = os.path.join(os.path.dirname(__file__), filename)
+        
+        
+    def get_all_notes(self) -> list:
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as file: 
+                return json.load(file)
+        except FileNotFoundError:
+            return []
     
-    # ! get all notes from json file
+
+    def get_note_by_id(self, note_id: str) -> dict:
+        notes = self.get_all_notes()
+        return next((note for note in notes if note['id'] == note_id), None)
+
     
-    # ! get note by id from json file
-    
-    # ! create a note
+    def create_note(self, note: NoteSchema) -> dict:
+        notes = self.get_all_notes()
+        new_note = note.model_dump()
+        new_note['id'] = str(uuid4())
+        notes.append(new_note)
+        self._save_notes(notes=notes)
+        return new_note
     
     # ! update a existing note
     
     # ! delete a existong note
     
-    # ! save the changes
+    
+    def _save_notes(self, notes):
+        for note in notes: 
+            note['id'] = str(note['id'])
+        with open(self.filename, 'w', encoding='utf-8') as file:
+            json.dump(notes, file, ensure_ascii=False, indent=4)
