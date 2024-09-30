c00ff980824ee70d61e6e385ebdd4134f2c8454f
maxitech <derstadlmax@gmail.com>
2024-09-30 10:58:54 +0200

    Added CORS middleware to get acces to the api in the frontend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
