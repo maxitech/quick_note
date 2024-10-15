c74b8e1de7e8379fe49e175fe248c2a2dc834ff3
maxitech <derstadlmax@gmail.com>
2024-10-15 18:02:57 +0200

    Prototype implementation of note manager and changed toggle styles for dropdown menu

--- frontend/src/renderer/src/navbar.ts
+++ frontend/src/renderer/src/navbar.ts
@@ -1,12 +1,21 @@
 const dropdownBtn = document.getElementById('dropdown-btn') as HTMLButtonElement
 const dropdownMenu = document.getElementById('dropdown-menu') as HTMLDivElement
 const categoryBtn = document.getElementById('category-btn') as HTMLButtonElement
+const notebookBtn = document.getElementById('notebook-btn') as HTMLButtonElement
 const notesContainer = document.getElementById('notes-container') as HTMLDivElement
+const noteBar = document.getElementById('note-bar') as HTMLDivElement
 
 dropdownBtn.addEventListener('click', () => {
   dropdownMenu.classList.toggle('hidden')
+  dropdownBtn.classList.toggle('nav-dropdown-shadow')
 })
 
 categoryBtn.addEventListener('click', () => {
   notesContainer.classList.toggle('hidden')
+  noteBar.classList.add('hidden')
+})
+
+notebookBtn.addEventListener('click', () => {
+  noteBar.classList.toggle('hidden')
+  notesContainer.classList.add('hidden')
 })
