639384a6c525e5f83bc5ac530ad3377dfdcd22cc
maxitech <derstadlmax@gmail.com>
2024-10-16 17:42:51 +0200

    Added top bar with button for adding a new notebook(no logic yet) in the notebook view.

--- frontend/src/renderer/src/navbar.ts
+++ frontend/src/renderer/src/navbar.ts
@@ -4,6 +4,7 @@ const categoryBtn = document.getElementById('category-btn') as HTMLButtonElement
 const notebookBtn = document.getElementById('notebook-btn') as HTMLButtonElement
 const notesContainer = document.getElementById('notes-container') as HTMLDivElement
 const noteBar = document.getElementById('note-bar') as HTMLDivElement
+const notebookTopBar = document.getElementById('notebook-top-bar') as HTMLDivElement
 
 dropdownBtn.addEventListener('click', () => {
   dropdownMenu.classList.toggle('hidden')
@@ -13,9 +14,11 @@ dropdownBtn.addEventListener('click', () => {
 categoryBtn.addEventListener('click', () => {
   notesContainer.classList.toggle('hidden')
   noteBar.classList.add('hidden')
+  notebookTopBar.classList.add('hidden')
 })
 
 notebookBtn.addEventListener('click', () => {
   noteBar.classList.toggle('hidden')
   notesContainer.classList.add('hidden')
+  notebookTopBar.classList.toggle('hidden')
 })
