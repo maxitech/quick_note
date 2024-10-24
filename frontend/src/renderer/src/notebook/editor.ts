0b8e0bbefde63a090e98ec46ae3b6ffbeb5e3460
maxitech <derstadlmax@gmail.com>
2024-10-24 16:24:05 +0200

    Refactored the Quill editor setup by simplifying imports and organizing the notebook fetching and preview generation logic into dedicated functions. This improves the clarity and functionality of the code, ensuring the editor correctly displays the content from the fetched notebook

--- frontend/src/renderer/src/notebook/editor.ts
+++ frontend/src/renderer/src/notebook/editor.ts
@@ -1,5 +1,10 @@
 import Quill from 'quill'
+import { Delta } from 'quill/core'
 import 'quill/dist/quill.snow.css'
+import getNotebook from '../../../api/notebooks/getNotebook'
+import getNotebooks from '../../../api/notebooks/getAllNotebooks'
+
+const noteBar = document.getElementById('note-bar') as HTMLDivElement
 
 const quill = new Quill('#editor', {
   modules: {
@@ -9,3 +14,54 @@ const quill = new Quill('#editor', {
   placeholder: 'Compose an epic...',
   theme: 'snow'
 })
+
+function generateNotebookPreview(id: string, title: string, content: string): void {
+  const notebookPreview = document.createElement('button')
+  notebookPreview.classList.add('note-bar-preview')
+  notebookPreview.dataset.id = id
+
+  notebookPreview.innerHTML = `
+    <h2 title="${title}">${title}</h2>
+    <p>${content}</p>
+  `
+
+  noteBar?.insertBefore(notebookPreview, noteBar.firstChild)
+}
+
+async function fetchNotebooks(): Promise<void> {
+  const notebooks = await getNotebooks()
+  noteBar.innerHTML = ''
+  notebooks.forEach((notebook) => {
+    generateNotebookPreview(notebook.id, notebook.title, notebook.content.ops[0].insert)
+  })
+}
+fetchNotebooks()
+
+let openNotebookId: string | null = null
+
+function handleNotebookClick(e: MouseEvent): void {
+  const target = e.target as HTMLElement
+  const notebookPreview = target.closest('.note-bar-preview') as HTMLButtonElement
+
+  if (notebookPreview) {
+    e.stopPropagation()
+    const clickedNotebookId = notebookPreview.dataset.id as string
+
+    if (openNotebookId === clickedNotebookId) return
+
+    openNotebookId = clickedNotebookId
+
+    openNotebook(clickedNotebookId)
+  }
+}
+
+noteBar.addEventListener('click', handleNotebookClick)
+
+async function openNotebook(id: string): Promise<void> {
+  const notebook = await getNotebook(id)
+  if (!notebook) return
+
+  const delta = notebook.content as Delta
+  quill.setContents(delta)
+  quill.focus()
+}
