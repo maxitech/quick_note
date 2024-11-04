6ca10cef19fd1a0cc0e4906a80709dc7803e8605
maxitech <derstadlmax@gmail.com>
2024-11-04 12:32:58 +0100

    refactor: Remove unused dropdown menu code and update button toggle functionality
    - Removed dropdown menu code as it's not needed for the current implementation.
    - Updated button functionality to ensure 'stickyNoteBtn' is active by default on load.
    - Simplified toggle logic for 'stickyNoteBtn' and 'notebookBtn' to streamline button state management.

--- frontend/src/renderer/src/notes/navbar.ts
+++ frontend/src/renderer/src/notes/navbar.ts
@@ -1,25 +1,29 @@
-const dropdownBtn = document.getElementById('dropdown-btn') as HTMLButtonElement
-const dropdownMenu = document.getElementById('dropdown-menu') as HTMLDivElement
 const stickyNoteBtn = document.getElementById('sticky-note-btn') as HTMLButtonElement
 const notebookBtn = document.getElementById('notebook-btn') as HTMLButtonElement
 const notesContainer = document.getElementById('notes-container') as HTMLDivElement
 const notebookContainer = document.getElementById('notebook-container') as HTMLDivElement
 
-dropdownBtn.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('hidden')
-  dropdownBtn.classList.toggle('nav-dropdown-shadow')
-})
+// ? Code for a dropdown menu if needed in the future
+// const dropdownBtn = document.getElementById('dropdown-btn') as HTMLButtonElement
+// const dropdownMenu = document.getElementById('dropdown-menu') as HTMLDivElement
+
+// dropdownBtn.addEventListener('click', () => {
+//   dropdownMenu.classList.toggle('hidden')
+//   dropdownBtn.classList.toggle('nav-dropdown-shadow')
+// })
+
+stickyNoteBtn.classList.add('btn-active')
 
 stickyNoteBtn.addEventListener('click', () => {
-  notesContainer.classList.toggle('hidden')
+  stickyNoteBtn.classList.add('btn-active')
+  notesContainer.classList.remove('hidden')
   notebookContainer.classList.add('hidden')
-  stickyNoteBtn.classList.toggle('btn-active')
   notebookBtn.classList.remove('btn-active')
 })
 
 notebookBtn.addEventListener('click', () => {
   notesContainer.classList.add('hidden')
-  notebookContainer.classList.toggle('hidden')
+  notebookContainer.classList.remove('hidden')
   notebookBtn.classList.toggle('btn-active')
   stickyNoteBtn.classList.remove('btn-active')
 })
