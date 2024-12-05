819ce4f4e245ca9eec82712ddec7ba16dd00f288
maxitech <derstadlmax@gmail.com>
2024-12-05 16:01:04 +0100

    refactor: Extracted `get_resource_path` from `Repository` class into a standalone utility function and updated the `Repository` class accordingly.

---
+++ backend/app/db/uitls/file_utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import json
+
+def get_resource_path(filename: str) -> str:
+    if getattr(sys, 'frozen', False):
+        if sys.platform == "win32":
+            base_path = os.path.join(os.path.expanduser("~"), "AppData", "Roaming", "quick_notes")
+        elif sys.platform == "darwin":
+            base_path = os.path.join(os.path.expanduser("~"), "Library", "Application Support", "quick_notes")
+        else:
+            base_path = os.path.join(os.path.expanduser("~"), ".local", "share", "quick_notes")
+    else:
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "db"))
+        
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+            
+    file_path = os.path.join(base_path, filename)
+        
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as f:
+            json.dump([], f) 
+                
+    return file_path
