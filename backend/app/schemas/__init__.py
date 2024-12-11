74ee189b82538debc2eeb87254a7b67bb224f968
maxitech <derstadlmax@gmail.com>
2024-12-11 21:18:02 +0100

    update: Add `SettingsSchema` import.

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .note_schema import NoteSchema
 from .note_update_schema import NoteUpdateSchema
 from .notebook_schema import NotebookSchema
 from .notebook_update_schema import NotebookUpdateSchema
+from .settings_schema import SettingsSchema
