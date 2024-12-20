e6c37fff8bd3124f1f4d2062679c4b8275c90490
maxitech <derstadlmax@gmail.com>
2024-12-20 17:23:23 +0100

    update: Enhance Settings Router and Update Initialization Logic
    - Changed the function name from `get_notebooks` to `get_settings` for clarity and consistency.
    - Modified the update settings route to handle both `ColorSchemaUpdate` and `ActiveThemeUpdate` payloads using `Union` for type-safety.
    - Updated the endpoint path to `/settings/update` to clearly distinguish between updating settings and retrieving them.

--- backend/app/api/settings_routes.py
+++ backend/app/api/settings_routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from typing import Union
+
 from db import SettingsRepository
-from db.uitls import default_settings
-from schemas import SettingsSchema
+from db.utils import default_settings
+from schemas import SettingsSchema, ColorSchemaUpdate, ActiveThemeUpdate
 
 
 settings_repo = SettingsRepository('settings.json')
@@ -13,15 +15,15 @@ settings_repo.initialize_settings_with_defaults(default_settings)
 settings_router = APIRouter()
 
 
-@settings_router.patch('/settings', response_model=SettingsSchema)
-def update_settings(payload: SettingsSchema): 
+@settings_router.patch("/settings/update", response_model=SettingsSchema)
+def update_settings(payload: Union[ColorSchemaUpdate, ActiveThemeUpdate]):
     settings_repo.initialize_settings_with_defaults(default_settings)
     updated_settings = settings_repo.update_settings(payload)
     return updated_settings
 
 
 @settings_router.get('/settings', response_model=SettingsSchema)
-def get_notebooks():
+def get_settings():
     settings_repo.initialize_settings_with_defaults(default_settings)
     settings = settings_repo.get_settings()
     return settings
