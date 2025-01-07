e1c6928a4eb84873f19ad576886d8c9e7ac00f5c
maxitech <derstadlmax@gmail.com>
2025-01-07 11:30:32 +0100

    update: Extended `themes` with more colors for better customization options of a theme.

--- backend/app/db/utils/default_settings.py
+++ backend/app/db/utils/default_settings.py
@@ -4,12 +4,26 @@ DEFAULT_SETTINGS = {
   "colorSchema": ["#7afcff", "#ff7eb9"],
   "themes": {
     "light": {
-      "backgroundColor": "#dee2e6",
-      "textColor": "#212529"
+      "backgroundColor": "#fff",
+      "textColor": "#212529",
+      "notebarBgColor": "#f8f9fa",
+      "editorBgColor": "#fff",
+      "hoverColor": "#b8bee667",
+      "settingsIconColor": "#6c757d",
+      "settingsBgHoverColor": "#b8bee667",
+      "settingsBgActiveColor": "#a3a8ce67",
+      "settingsInsideBorderColor": "#bdb2ff"
     },
     "dark": {
       "backgroundColor": "#343a40",
-      "textColor": "#dee2e6"
+      "textColor": "#dee2e6",
+      "notebarBgColor": "#212529",
+      "editorBgColor": "#dee2e6",
+      "hoverColor": "#495057",
+      "settingsIconColor": "#adb5bd",
+      "settingsBgHoverColor": "#2b2d3b67",
+      "settingsBgActiveColor": "#2b2d3b85",
+      "settingsInsideBorderColor": "#007e8f"
     }
   },
   "defaultTheme": "light",
